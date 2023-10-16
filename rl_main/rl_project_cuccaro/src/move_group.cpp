4fadcb5114a3516d6de60ea0129d81fef4e20c9a
Fabio <fabio@cuccaro.eu>
2023-10-16 17:35:54 +0200

    first commit

---
+++ rl_main/rl_project_cuccaro/src/move_group.cpp
@@ -0,0 +1,107 @@
+#include "myHeader.h"
+
+
+static const std::string PLANNING_GROUP = "kuka_iiwa_arm";
+
+
+MOVEIT_CLASS::MOVEIT_CLASS() : _move_group_interface(PLANNING_GROUP) {
+
+    //move to default position specified in the moveit wizard (look_down_pose)
+    _move_group_interface.setPoseReferenceFrame("map");
+    _move_group_interface.setJointValueTarget(_move_group_interface.getNamedTargetValues("look_down_pose"));
+    _move_group_interface.asyncMove(); // a differenza di move(), asyncMove() is not blocking
+
+    _aruco_sub = _nh.subscribe("/kuka_iiwa/aruco_marker_publisher_kuka_iiwa/markers", 1, &MOVEIT_CLASS::aruco_cb, this);
+    _diff_drive_sub = _nh.subscribe("/diff_drive_topic", 1, &MOVEIT_CLASS::diff_drive_cb, this);
+
+    _enable_motion = false;
+    _stop_motion = false;
+    _exit_from_loop = false;
+    _task_completed = false;
+}
+
+
+void MOVEIT_CLASS::aruco_cb(const aruco_msgs::MarkerArrayConstPtr &marker){
+	_marker_id = marker->markers[0].id;
+    if(_marker_id == _requested_marker){
+        _stop_motion = true;
+    }
+    else _stop_motion = false;
+
+    _marker_pose.position.x = marker->markers[0].pose.pose.position.x;
+    _marker_pose.position.y = marker->markers[0].pose.pose.position.y;
+
+}
+
+
+void MOVEIT_CLASS::diff_drive_cb(std_msgs::Int32 req_marker){ //ci entra
+    if(req_marker.data > 0){
+        _enable_motion = true;
+        _requested_marker = req_marker.data;
+    }
+    else _enable_motion = false;
+}
+
+
+
+void MOVEIT_CLASS::ctrl_loop(){
+
+    ros::Rate rate(10);
+    while(ros::ok() && !_task_completed){
+        if(_enable_motion && !_stop_motion){
+            
+            //rotate joint 3. While rotating, stop if marker is found
+            _current_state = _move_group_interface.getCurrentState();
+            _joint_model_group = _move_group_interface.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+            _current_state->copyJointGroupPositions(_joint_model_group, _joint_group_positions);
+            _joint_group_positions[2] = -2.9670;  // turn joint 3 from -2.9670 rad to 2.9670 rad
+            _move_group_interface.setJointValueTarget(_joint_group_positions);
+            
+            _move_group_interface.asyncMove();
+            //_move_group_interface.move();
+
+            //stop if marker is found
+            while(!_exit_from_loop){
+                if(_stop_motion){
+                    _move_group_interface.stop();
+                    _exit_from_loop = true;
+                }
+                rate.sleep();
+            }
+
+            //execute the motion to reach the marker
+            geometry_msgs::Pose goal_pose;
+            goal_pose.position.x = _marker_pose.position.x;
+            goal_pose.position.y = _marker_pose.position.y;
+            goal_pose.position.z = 0.1;
+            goal_pose.orientation.w = 0.0;
+            goal_pose.orientation.x = 0.0;
+            goal_pose.orientation.y = -1.0;
+            goal_pose.orientation.z = 0.0;
+
+            //_move_group_interface.setPoseReferenceFrame("map");
+            _move_group_interface.setPoseTarget(goal_pose);
+            _move_group_interface.move();
+            _move_group_interface.stop();
+            _task_completed = true;
+            
+        }
+        
+    }
+    rate.sleep();
+}
+
+
+void MOVEIT_CLASS::run(){
+    boost::thread ctrl_loop_t( &MOVEIT_CLASS::ctrl_loop, this);
+    ros::spin();
+}
+
+
+int main(int argc, char** argv){
+    ros::init(argc, argv, "move_group_main");
+    MOVEIT_CLASS mov;
+    mov.run(); 
+
+    return 0;
+}
---
+++ rl_main/rl_project_cuccaro/src/myHeader.h
@@ -0,0 +1,128 @@
+#include "ros/ros.h" 
+
+//move_base libraries
+#include <move_base_msgs/MoveBaseAction.h> 
+#include <actionlib/client/simple_action_client.h>
+
+//#include "ros_service/service.h"
+
+//thread library
+#include "boost/thread.hpp" //to have the control loop running
+
+#include <iostream>
+#include <sstream>
+
+#include "std_msgs/Int32.h"
+#include "std_msgs/Bool.h"
+#include <std_msgs/UInt32MultiArray.h>
+#include <std_msgs/Float64.h>
+#include "geometry_msgs/Twist.h" //to command the robot in velocity
+#include "geometry_msgs/PoseStamped.h"
+#include "nav_msgs/Odometry.h" //to read the robot pose
+#include "tf/tf.h" //to use the tf package (specifically, I want to use getRPY() )
+#include <tf/transform_listener.h>
+
+
+//MoveIt Libraries
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+
+#include <moveit_msgs/AttachedCollisionObject.h>
+#include <moveit_msgs/CollisionObject.h>
+
+#include <moveit_visual_tools/moveit_visual_tools.h>
+
+//aruco library
+
+#include <aruco_msgs/MarkerArray.h>
+#include <aruco/aruco.h>
+
+#include "sensor_msgs/JointState.h"
+#include <cstdlib>
+#include <kdl_parser/kdl_parser.hpp>
+#include <kdl/chainfksolverpos_recursive.hpp>
+#include <kdl/chainiksolvervel_pinv.hpp>
+#include <kdl/chainfksolverpos_recursive.hpp>
+#include <kdl/chainiksolverpos_nr.hpp>
+
+
+using namespace std;
+
+
+class NAVIGATION{
+    public:
+        NAVIGATION();
+        void run();
+        
+        void aruco_cb(const aruco_msgs::MarkerArrayConstPtr &marker);
+        void odom_cb(const nav_msgs::OdometryConstPtr &odom);
+        
+        bool navigation(float x_goal, float y_goal); //It outputs 1 when the next waypoint is reached
+        void human_input();
+
+        //Threads
+        void ctrl_loop();
+        void diff_drive_loop();      
+
+    private:
+        ros::NodeHandle _nh;
+        
+        geometry_msgs::Point _curr_pos;
+        double _curr_yaw;
+        
+        int _marker_id;
+        int _requested_marker;
+        int _kukamark;
+        bool _marker_match;
+
+        bool _last_room;
+        bool _task_completed;
+
+        int _wp_index;
+        vector<geometry_msgs::Point> _waypoints; //rooms' centers' coordinates
+
+        ros::Publisher _cmd_vel_pub;
+        ros::Publisher _diff_drive_pub;
+
+        ros::Subscriber _aruco_sub;
+        ros::Subscriber _odom_sub;
+
+        actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> _ac;
+
+};
+
+
+class MOVEIT_CLASS{
+    public:
+        MOVEIT_CLASS();
+        void run();
+
+        void aruco_cb(const aruco_msgs::MarkerArrayConstPtr &marker);
+        void diff_drive_cb(std_msgs::Int32 req_marker); //used to receive the integer which is the marker requested by the user
+
+        void ctrl_loop();
+
+    private:
+        ros::NodeHandle _nh;
+
+        int _marker_id;
+        int _requested_marker;
+        geometry_msgs::Pose _marker_pose;
+        
+        bool _enable_motion;
+        bool _stop_motion;
+        bool _exit_from_loop;
+        bool _task_completed;
+
+        moveit::planning_interface::MoveGroupInterface _move_group_interface;
+        const moveit::core::JointModelGroup* _joint_model_group;
+        moveit::core::RobotStatePtr _current_state;
+        std::vector<double> _joint_group_positions;
+
+        ros::Subscriber _aruco_sub;
+        ros::Subscriber _diff_drive_sub;
+
+};
